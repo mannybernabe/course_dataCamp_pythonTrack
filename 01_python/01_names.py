af71ac58cbb8057f9d614506273d0c7d5cfb33b5
Manny Bernabe <manny.bernabe@uptake.com>
2019-02-15 20:36:36 -0600

    thru course 7 of 22

---
+++ 01_python/01_names.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+# Definition of savings and result
+savings = 100
+result = 100 * 1.10 ** 7
+
+# Fix the printout
+print("I started with $" + str(savings) + " and now have $" + str(result) + ". Awesome!")
+
+# Definition of pi_string
+pi_string = "3.1415926"
+
+# Convert pi_string into float: pi_float
+pi_float=float(pi_string)
---
+++ README.md
@@ -0,0 +1,1 @@
+# course_dataCamp_pythonTrack
